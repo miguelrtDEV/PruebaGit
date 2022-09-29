52fdc6e003f12a21fe5d7d19050ebbef2dc3ff72
miguelrtDEV <miguelrubiotardieu_7244@escuelaces.net>
2022-09-29 13:17:19 +0200

    Este es mi primer commit

---
+++ MiClasePrueba.java
@@ -0,0 +1,13 @@
+package es.miguelrtdev.pruebaGit;
+
+/**
+ * Esta clase es para probar que git funciona
+ * @author RogueGarnet
+ *
+ */
+public class MiClasePrueba {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+	}
+}
